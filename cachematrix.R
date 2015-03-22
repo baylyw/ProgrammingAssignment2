850afc66b1875a28b717f91b783db23e3a66ea4b
baylyw <baylyw@gmail.com>
2015-03-22 11:58:27 -0700

    “message”

--- cachematrix.R
+++ cachematrix.R
@@ -6,28 +6,28 @@
 ## and are important for determining whether the inverse has already been calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-  # inv <- NULL sets inverse value to NULL as a placeholder
-  inv <- NULL
-  # defines a function to set the matrix, x, to a new matrix, y, and resets the inverse, inv, to NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  # returns the matrix, x
-  get <- function(){
-    x 
-  } 
-  # sets the inverse, inv to inverse
-  setInv <- function(inverse){
-    inv <<- inverse
-  } 
-  getInv <- function() {
-    inv
-  }
-  # returns the 'special vector' containing all of the functions just defined.
-  list(set = set, get = get,
-       setInv = setInv,
-       getInv = getInv)
+        # inv <- NULL sets inverse value to NULL as a placeholder
+        inv <- NULL
+        # defines a function to set the matrix, x, to a new matrix, y, and resets the inverse, inv, to NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        # returns the matrix, x
+        get <- function(){
+                x 
+        } 
+        # sets the inverse, inv to inverse
+        setInv <- function(inverse){
+                inv <<- inverse
+        } 
+        getInv <- function() {
+                inv
+        }
+        # returns the 'special vector' containing all of the functions just defined.
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
@@ -35,20 +35,20 @@ makeCacheMatrix <- function(x = matrix()) {
 ## and reports either the new or cached inverse. 
 
 cacheSolve <- function(x, ...) {
-  # x is a list of functions and the matrix from makeCacheMatrix
-  # assign to inv in cacheSolve, the value from getInv(inv) 
-  inv <- x$getInv()
-  #If the inverse stored under the parameters "matrix x" is not NULL, return it.
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  # assign to data the vector x
-  data <- x$get()
-  # Calculate the inverse and assign it to inv
-  inv <- solve(data, ...)
-  # Store the inverse under the parameters "matrix x".
-  x$setInv(inv)
-  ## Return a matrix that is the inverse of 'x'
-  inv     
+        # x is a list of functions and the matrix from makeCacheMatrix
+        # assign to inv in cacheSolve, the value from getInv(inv) 
+        inv <- x$getInv()
+        #If the inverse stored under the parameters "matrix x" is not NULL, return it.
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        # assign to data the vector x
+        data <- x$get()
+        # Calculate the inverse and assign it to inv
+        inv <- solve(data, ...)
+        # Store the inverse under the parameters "matrix x".
+        x$setInv(inv)
+        ## Return a matrix that is the inverse of 'x'
+        inv     
 }
